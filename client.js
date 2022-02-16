9cf8a36b3137999d36086f9d22abd553e3a4803a
Ethan Loewen <ethanloewen@gmail.com>
2022-02-16 23:14:35 +0000

    Created an external file for constants

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
-const net = require("net");
+const net = require('net');
+const { IP, PORT } = require('./constants');
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541'
+    host: IP,
+    port: PORT
   });
-  conn.setEncoding("utf8");
+  conn.setEncoding('utf8');
 
   conn.on('data', (data) => {
     console.log(data);
